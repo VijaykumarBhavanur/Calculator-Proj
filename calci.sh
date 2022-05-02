68766d4c95dbf8a8d56ab8a6c719b2e868ac74ad
VijaykumarBhavanur <44596275+VijaykumarBhavanur@users.noreply.github.com>
2022-05-02 09:20:38 +0530

    Update calci.sh

--- calci.sh
+++ calci.sh
@@ -1,18 +1,24 @@
 echo "Welcome to calculator!"
 
+
+echo "Below is addition code"
 num1=10
 num2=20
 sum=num1 + num2
 
+
+echo "Below is substraction code"
 num1=10
 num2=20
 sub=num1 - num2
 
+echo "Below is multiplication code"
 
 num1=10
 num2=20
 mul=num1 * num2
 
+echo "Below is division code"
 num1=10
 num2=20
 div=num1 / num2
